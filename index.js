d4bf3e23c507d29c21f1e0311f1ce1fd9e6e56e7
MonoDG <monodg@proton.me>
2023-12-11 20:03:31 -0500

    Add dropdown

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const dropdownButton = document.querySelector("#dropdown");
+const dropdownContentDiv = document.querySelector("#dropdown-content");
+
+const anotherDropdownButton = document.querySelector("#dropdown2");
+const anotherDropdownContentDiv = document.querySelector("#dropdown-content2");
+
+dropdownButton.addEventListener("click", () => {
+  toggleVisibility(dropdownButton, dropdownContentDiv);
+});
+
+anotherDropdownButton.addEventListener("click", () => {
+  toggleVisibility(anotherDropdownButton, anotherDropdownContentDiv);
+});
+
+function toggleButtonText(button, content) {
+  button.textContent = content.classList.contains("hidden")
+    ? "Collapse -"
+    : "Expand +";
+}
+
+function toggleVisibility(button, content) {
+  toggleButtonText(button, content);
+  content.classList.toggle("hidden");
+}
